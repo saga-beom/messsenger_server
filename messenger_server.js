5e5711ef7ffa33856182acefd52b6582506af6ab
Seo Hyoboem <shb0217@naver.com>
2023-02-02 06:55:59 +0900

    Modify Post api to serve json data

--- messenger_server.js
+++ messenger_server.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 const sqlite3 = require("sqlite3").verbose();
+const bodyParser = require('body-parser');
 
-let db = new sqlite3.Database('./Account.db',sqlite3.OPEN_READWRITE,(err) => {
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extented: true}))
+
+const db = new sqlite3.Database('./Account.db',sqlite3.OPEN_READWRITE,(err) => {
   if(err) {
     console.error(err.message);
   } else {
@@ -11,9 +15,21 @@ let db = new sqlite3.Database('./Account.db',sqlite3.OPEN_READWRITE,(err) => {
   }
 })
 
-app.get('/', (req, res) => {
-  res.send("<h1>Hello world</h1>")
+app.post('/', (req, res) => {
+  console.log("POST API called")
+  db.run('INSERT INTO ACCOUNT(ID,PW,Nickname,Email) VALUES(?,?,?,?)', 
+  req.body.id.toLowerCase(), req.body.pwd, 
+  req.body.nickname, req.body.email.toLowerCase(), function (err) {
+    if (err) {
+      res.json({error : "overlap"})
+    }
+  });
 })
+
+app.get('/', (req, res) => { // 중복된 아이디 있을 때 API 넘겨주기 
+  console.log("API call")
+  res.json({test : "test"})
+}) 
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
