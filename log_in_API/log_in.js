01f60438854654c11a9d63e90c35fd49f8ffb74a
saga-beom <hyobeom003@gmail.com>
2023-02-06 01:50:56 +0900

    account update

--- log_in_API/log_in.js
+++ log_in_API/log_in.js
@@ -1,18 +1,9 @@
 const express = require('express')
 const router = express.Router();
-// const sqlite3 = require("sqlite3").verbose()
 const bodyParser = require('body-parser')
 
 router.use(bodyParser.json())
 router.use(bodyParser.urlencoded({extented: true}))
-
-// const db = new sqlite3.Database('./Account.db',sqlite3.OPEN_READWRITE,(err) => {
-//     if(err) {
-//       console.error(err.message);
-//     } else {
-//       console.log('connected to the mydb database to logIn API');
-//     }
-// })
 
 router.get('/:id', (req, res) => {
 
