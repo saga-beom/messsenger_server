281d91e7dadf11a2030d84226f66f35dd032d81f
saga-beom <hyobeom003@gmail.com>
2023-02-26 05:12:07 +0900

    socket network
    make socket communication environment

--- get_freind_list/get_friend_list.js
+++ get_freind_list/get_friend_list.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const bodyParser = require('body-parser')
 
 router.use(bodyParser.json())
-router.use(bodyParser.urlencoded({extented: true}))
+router.use(bodyParser.urlencoded({extended: true}))
 
 router.get('/:myId', (req, res) => {
 
@@ -11,13 +11,12 @@ router.get('/:myId', (req, res) => {
 
     const myId = req.params.myId.toLowerCase()
 
-    db.all('SELECT FriendNickname FROM FRIENDSRELATIONSHIP WHERE MyID = ?', [myId], (err, rows) => {
+    db.all('SELECT FriendID, FriendNickname FROM FRIENDSRELATIONSHIP WHERE MyID = ?', [myId], (err, rows) => {
         if(err) {
             console.error(err.message)
         } else {
-            const nicknames = rows.map(row => row.FriendNickname);
-            console.log(nicknames);
-            res.json({ nicknames });
+            const friends = rows.map(row => ({ friendId: row.FriendID, friendNickname: row.FriendNickname}))
+            res.send(friends);
         }
     }) 
 
--- messenger_server.js
+++ messenger_server.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const http = require('http').createServer(app)
 const sqlite3 = require("sqlite3").verbose()
+const io = require('socket.io')(http)
 
 const setUpAccountRouterAPI = require('./creat_account_API/account')
 const logInAPI = require('./log_in_API/log_in')
 const searchFriendAPI = require('./search_friend/search_friend')
 const getFriendList = require('./get_freind_list/get_friend_list')
+
+const userSockets = {};
 
 app.use("/setUpAccount", setUpAccountRouterAPI)
 app.use("/logIn", logInAPI)
@@ -21,11 +25,38 @@ global.db = new sqlite3.Database('./Account.db',sqlite3.OPEN_READWRITE,(err) => 
   }
 })
 
+io.on('connection', (socket) => {
+  console.log('a user connected')
+
+  socket.on('register', (userId) => {
+    userSockets[userId] = socket.id
+    console.log(`user ${userId} registered with socket id ${socket.id}`)
+  })
+
+  socket.on('chat message', (data) => {
+    const receiverSocketId = userSockets[data.receiver]
+    console.log('sender: ' + data.sender.toLowerCase());
+    console.log('receiver: ' + data.receiver);
+    console.log('msg: '+ data.msg);
+    if (receiverSocketId) {
+      io.to(userSockets[data.sender.toLowerCase()]).emit('chat message', data)
+      io.to(receiverSocketId).emit('chat message', data)
+    } else {
+      console.log(`receiver ${data.receiver} is not registered`)
+    }
+    
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected with socket id: ' + socket.id);
+  });
+})
+
+http.listen(port, () => {
+  console.log(`listening on *: 3000`)
+})
 
 app.get('/', (req, res) => {  
   res.send("Server is Operating")
 }) 
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
